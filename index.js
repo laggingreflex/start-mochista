7bb17a620e0ef15c7ef30a988735dae97ffe4a16
laggingreflex <laggingreflex@gmail.com>
2017-05-28 01:29:58 +0530

    fix: better config parsing inline with fixes in mochista

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
 module.exports = config => input => function mochista() {
-  return require('mochista').default(config)
+  const mochista = require('mochista').default;
+  const getMochaOptions = require('mochista/dist/config/mocha-options').default;
+  const merge = require('mochista/dist/config/merge').default;
+  const fix = require('mochista/dist/config/fix').default;
+  const mochaOptions = getMochaOptions();
+  config = fix(merge(mochaOptions, config));
+  return mochista(config)
     .then(({ run }) => run());
 }
